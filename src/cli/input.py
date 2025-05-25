706b7a2ce62862c75a74aa376cfc421ffa321383
Jana Klar <jana-klar123@protonmail.com>
2025-05-25 19:23:53 +0200

    feat(op-select, core): integrate option selection in cli main

--- src/cli/input.py
+++ src/cli/input.py
@@ -1,3 +1,5 @@
+from src.cli.operations import get_operation_from_user
+
 def get_number_from_input(message:str = "Enter a number: ") -> int:
 
     while True:
@@ -10,7 +12,8 @@ def get_number_from_input(message:str = "Enter a number: ") -> int:
 
 def cli_main():
     print("Welcome to the CLI Calculator")
-    
+
     a = get_number_from_input("Enter the first number: ")
     b = get_number_from_input("Enter the second number: ")
-    # TODO: let user choose operation
+    operation = get_operation_from_user()
+    print(f"The result of {operation.name} with {a} and {b} is {operation.func(a, b)}") 
